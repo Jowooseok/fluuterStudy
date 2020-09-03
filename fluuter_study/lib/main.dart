9a2acc0eed8c85faac0051f16c7cacbf74bd2553
Jowooseok <whdntjrsla12@naver.com>
2020-09-04 02:13:32 +0900

    complete

--- fluuter_study/lib/main.dart
+++ fluuter_study/lib/main.dart
@@ -9,16 +9,17 @@ class Myapp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Color(0xff607D8B),
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CircleAvatar(
                 radius: 50.0,
                 backgroundImage: AssetImage('images/wooseok.jpg'),
               ),
               Text(
-                'Jrun2ng',
+                'WooSeok',
                 style: TextStyle(
                   fontFamily: 'Pacifico',
                   fontSize: 40.0,
@@ -30,12 +31,51 @@ class Myapp extends StatelessWidget {
                 'FLUUTER DEVELOPER',
                 style: TextStyle(
                   fontFamily: 'Source Sans Pro',
-                  color: Colors.teal.shade100,
+                  color: Color(0xffCFD8DC),
                   fontSize: 20.0,
                   letterSpacing: 2.5,
                   fontWeight: FontWeight.bold,
                 ),
-              )
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Color(0xff9E9E9E),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Color(0xff757575),
+                    ),
+                    title: Text(
+                      '010-9529-8640',
+                      style: TextStyle(
+                        color: Color(0xff212121),
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                      ),
+                    )),
+              ),
+              Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        color: Color(0xff757575),
+                      ),
+                      title: Text(
+                        'jrun2ng@gmail.com',
+                        style: TextStyle(
+                          color: Color(0xff212121),
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20.0,
+                        ),
+                      ))),
             ],
           ),
         ),

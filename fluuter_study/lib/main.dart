c01025b36e09a3a280763a037d479e3cf253f091
Jowooseok <whdntjrsla12@naver.com>
2020-09-09 01:17:54 +0900

    complete bossChallenge

--- fluuter_study/lib/main.dart
+++ fluuter_study/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
@@ -28,12 +29,17 @@ class Ball extends StatefulWidget {
 class _BallState extends State<Ball> {
   @override
   Widget build(BuildContext context) {
+    int ballNumber = 1;
+
     return Center(
       child: FlatButton(
         onPressed: () {
-          print('hello');
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+          print(ballNumber);
         },
-        child: Image.asset('images/ball1.png'),
+        child: Image.asset('images/ball$ballNumber.png'),
       ),
     );
   }

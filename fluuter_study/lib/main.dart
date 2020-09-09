d6a2fafc41e279333e4ed7ea007bfd69c167bafe
Jowooseok <whdntjrsla12@naver.com>
2020-09-09 23:09:04 +0900

    start1

--- fluuter_study/lib/main.dart
+++ fluuter_study/lib/main.dart
@@ -9,17 +9,10 @@ class Myapp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-            margin: EdgeInsets.all(20.0),
-            color: Colors.white,
-            child: Text('Hello'),
-          ),
-        ),
-      ),
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+            child: Container(),
+          )),
     );
   }
 }

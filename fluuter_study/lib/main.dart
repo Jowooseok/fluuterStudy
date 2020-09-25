ac3c6b68d41ce3f8f8307d722e8f45d379bf6893
Jowooseok <whdntjrsla12@naver.com>
2020-09-25 18:47:32 +0900

    중복코드 함수로 전환

--- fluuter_study/lib/main.dart
+++ fluuter_study/lib/main.dart
@@ -1,18 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() {
   runApp(Myapp());
 }
 
 class Myapp extends StatelessWidget {
+  void playSound(String a) {
+    final player = new AudioCache();
+    player.play(a);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: Colors.teal,
           body: SafeArea(
-            child: Container(),
-          )),
+        child: Column(
+          children: [
+            FlatButton(
+              onPressed: () {
+                playSound('note1.wav');
+              },
+              color: Colors.red,
+            ),
+            FlatButton(
+              onPressed: () {
+                playSound('note2.wav');
+              },
+              color: Colors.orange,
+            ),
+            FlatButton(
+              onPressed: () {
+                playSound('note3.wav');
+              },
+              color: Colors.yellow,
+            ),
+            FlatButton(
+              onPressed: () {
+                playSound('note4.wav');
+              },
+              color: Colors.green,
+            ),
+            FlatButton(
+              onPressed: () {
+                playSound('note5.wav');
+              },
+              color: Colors.blue,
+            ),
+            FlatButton(
+              onPressed: () {
+                playSound('note6.wav');
+              },
+              color: Colors.blueAccent,
+            ),
+            FlatButton(
+              onPressed: () {
+                playSound('note7.wav');
+              },
+              color: Colors.purple,
+            ),
+          ],
+        ),
+      )),
     );
   }
 }

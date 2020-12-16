29ca34128b050ce36551c980899db21a5e67cd46
Jowooseok <whdntjrsla12@naver.com>
2020-12-17 01:23:10 +0900

    separated main dart files.

--- fluuter_study/lib/main.dart
+++ fluuter_study/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'imput_page.dart';
 
 void main() => runApp(BMICalculator());
 
@@ -6,35 +7,11 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
+      theme: ThemeData.dark().copyWith(
         primaryColor: Color(0xff0a0e21),
         scaffoldBackgroundColor: Color(0xff0a0e21),
-        textTheme: TextTheme(body1: TextStyle(color: Color(0xffffffff))),
-        accentColor: Colors.purple,
       ),
       home: InputPage(),
-    );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-      ),
     );
   }
 }

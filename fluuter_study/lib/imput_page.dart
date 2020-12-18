089d89426f007936c80638ea53d1f2043c9e1ecb
Jowooseok <whdntjrsla12@naver.com>
2020-12-18 22:11:32 +0900

    125. complete

--- fluuter_study/lib/imput_page.dart
+++ fluuter_study/lib/imput_page.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'reusable_card.dart';
+import 'icon_content.dart';
+
+const bottomContainerHeight = 80.0;
+const activeCardColour = Color(0xff1d1e33);
+const bottomContainerColour = Color(0xffeb1555);
 
 class InputPage extends StatefulWidget {
   @override
@@ -12,8 +19,58 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: [
+          Expanded(
+              child: Row(
+            children: [
+              Expanded(
+                child: ReusableCard(
+                  colour: activeCardColour,
+                  cardChild: IconContent(
+                    label: 'MALE',
+                    icon: FontAwesomeIcons.mars,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: ReusableCard(
+                  colour: activeCardColour,
+                  cardChild: IconContent(
+                    label: 'FEMALE',
+                    icon: FontAwesomeIcons.venus,
+                  ),
+                ),
+              ),
+            ],
+          )),
+          Expanded(
+            child: ReusableCard(
+              colour: activeCardColour,
+            ),
+          ),
+          Expanded(
+              child: Row(
+            children: [
+              Expanded(
+                child: ReusableCard(
+                  colour: activeCardColour,
+                ),
+              ),
+              Expanded(
+                child: ReusableCard(
+                  colour: activeCardColour,
+                ),
+              ),
+            ],
+          )),
+          Container(
+            color: bottomContainerColour,
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: bottomContainerHeight,
+          ),
+        ],
       ),
     );
   }
